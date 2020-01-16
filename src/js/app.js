c78515ed827c0e1d5e73d03ce34cdda03aa79bc0
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-16 23:50:34 +0300

    split handle input into multiple functions

--- src/js/app.js
+++ src/js/app.js
@@ -4,64 +4,54 @@ const formInput = document.querySelector('#input')
 
 const items = document.querySelector('#items')
 
-const handleInput = () => {
-  const option = formOption.value
-  let input = +formInput.value || 0
-  const result = []
-
-  if (input !== 0) {
-    items.classList.remove('items--hidden')
-  } else {
-    items.classList.add('items--hidden')
-  }
-
-  if (option === 'kg') {
-    result.push(
-      {
-        title: 'Pounds',
-        value: input * 2.2
-      },
-      {
-        title: 'Stones',
-        value: input * 0.16
-      },
-    )
-  } else if (option === 'lbs') {
-    formInput.placeholder = 'Enter pounds...'
-    input *= 0.45
-    
-    result.push(
-      {
-        title: 'Kilograms',
-        value: input
-      },
-      {
-        title: 'Stones',
-        value: input * 0.16
-      },
-    )
-  } else if (option === 'st') {
-    formInput.placeholder = 'Enter stones...'
-    input *= 6.35
-    
-    result.push(
-      {
-        title: 'Kilograms',
-        value: input
-      },
-      {
-        title: 'Pounds',
-        value: input * 2.2
-      },
-    )
-  }
-
+const shortenValues = (result) => {
   result.forEach(item => {
     if (item.value % 1 !== 0) {
       item.value = item.value.toFixed(2)
     }
   })
+}
 
+const toggleDisplayItems = (input) => {
+  if (input !== 0) {
+    items.classList.remove('items--hidden')
+  } else {
+    items.classList.add('items--hidden')
+  }
+}
+
+const getResult = (option, input) => {
+  if (option === 'kg') {
+    return [
+      { title: 'Pounds', value: input * 2.2 }, 
+      { title: 'Stones', value: input * 0.16 }
+    ]
+  } else if (option === 'lbs') {
+    formInput.placeholder = 'Enter pounds...'
+    input *= 0.45
+    
+    return [
+      { title: 'Kilograms', value: input },
+      { title: 'Stones', value: input * 0.16 }
+    ]
+  } else if (option === 'st') {
+    formInput.placeholder = 'Enter stones...'
+    input *= 6.35
+    
+    return [
+      { title: 'Kilograms', value: input },
+      { title: 'Pounds', value: input * 2.2 }
+    ]
+  }
+}
+
+const handleInput = () => {
+  const option = formOption.value
+  const input = +formInput.value || 0
+  const result = getResult(option, input)
+
+  toggleDisplayItems(input)
+  shortenValues(result)
   outputResult(result)
 }
 
